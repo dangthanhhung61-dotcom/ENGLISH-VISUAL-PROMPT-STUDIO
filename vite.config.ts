d047f8272c32f9de8f3570dbe5b6e7baea1cd3e2
dangthanhhung61-dotcom <dangthanhhung61@gmail.com>
2026-09-14 19:18:25 +0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/ENGLISH VISUAL PROMPT STUDIO/' ,
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
